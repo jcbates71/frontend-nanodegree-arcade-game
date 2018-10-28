4e67375fceab57a0eb2b9680809104ae7baf52ba
John Bates <waterdeep71@gmail.com>
2018-10-28 15:56:58 -0500

    Update enemy class
    Resetting handled by a method.
    Enemy resets after moving off screen.
    Also cleanup up indents.

--- js/app.js
+++ js/app.js
@@ -12,43 +12,42 @@ let allEnemies, player;
 let score = 0;
 
 // Enemies our player must avoid
-var Enemy = function(speed, leftToRight, row) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+var Enemy = function() {
+  // Variables applied to each of our instances go here,
+  // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-
-    // TODO: Add method for setting speed, leftToRight, and row.
-
-    this.speed = speed;
-    this.leftToRight = leftToRight;
-    if (leftToRight) {
-      this.x = -BOARD_COLUMN_WIDTH;
-    } else {
-      this.x = (BOARD_COLUMN_COUNT + 1) * BOARD_COLUMN_WIDTH;
-    }
-    this.row = row;
-    this.y = row;
+  // The image/sprite for our enemies, this uses
+  // a helper we've provided to easily load images
+  this.sprite = 'images/enemy-bug.png';
+  this.resetEnemy();
+  if (this.leftToRight) {
+    this.x = -BOARD_COLUMN_WIDTH;
+  } else {
+    this.x = (BOARD_COLUMN_COUNT + 1) * BOARD_COLUMN_WIDTH;
+  }
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    let movement = ENEMY_SPEED * this.speed * dt;
-    if (!this.leftToRight) {movement *= -1}
-    this.x += movement;
-    // TODO: Needs to reset after moving off the screen.
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
+  let movement = ENEMY_SPEED * this.speed * dt;
+  if (!this.leftToRight) {movement *= -1}
+  this.x += movement;
+  if (this.x < -BOARD_COLUMN_WIDTH || this.x > (BOARD_COLUMN_COUNT + 1) * BOARD_COLUMN_WIDTH) {this.resetEnemy()};
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
   // TODO: If the enemy is moving right to left, the sprite needs to be flipped.
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y * BOARD_ROW_HEIGHT);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y * BOARD_ROW_HEIGHT);
+};
+Enemy.prototype.resetEnemy = function () {
+  this.speed = Math.floor(Math.random() * 3) + 1;
+  this.leftToRight = Math.floor(Math.random() * 2) == 0;
+  this.y = Math.floor(Math.random() * 3) + 1;
 };
 
 // Now write your own player class
@@ -104,22 +103,19 @@ Player.prototype.moveToStartingPosition = function () {
 player = new Player();
 allEnemies = new Array();
 for (var i = 0; i < ENEMY_COUNT; i++) {
-  const randomSpeed = Math.floor(Math.random() * 3) + 1;
-  const randomDirection = Math.floor(Math.random() * 2) == 0;
-  const randomRow = Math.floor(Math.random() * 3) + 1;
-  allEnemies.push(new Enemy(randomSpeed, randomDirection, randomRow));
+  allEnemies.push(new Enemy());
 }
 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
